b6474e0fa1110c51ea1de3946cc85f34b64d92ff
unknown <thebtihamdi@gmail.com>
2026-03-23 10:16:56 +0100

    first commit

--- algo.js
+++ algo.js
@@ -1,43 +1,30 @@
-function sumDistinct(set1, set2) {
-    let sum = 0;
+Algorithm analyze_sentence
+VAR
+    INTEGER := 0;
+    word_counter : INTEGER := 1;
+    vowel_counter : INTEGER := 0;
+    char : CHAR;
+BEGIN
+    WRITE("Enter a character (end with a point): ");
+    READ(char);
 
-    for (let i = 0; i < set1.length; i++) {
-        let found = false;
-        for (let j = 0; j < set2.length; j++) {
-            if (set1[i] === set2[j]) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            sum = sum + set1[i];
-        }
-    }
+    WHILE (char <> '.') DO
+        length_counter := length_counter + 1;
 
-    for (let i = 0; i < set2.length; i++) {
-        let found = false;
-        for (let j = 0; j < set1.length; j++) {
-            if (set2[i] === set1[j]) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            sum = sum + set2[i];
-        }
-    }
-    return sum;
-}
+        IF (char = 'a' OR char = 'e' OR char = 'i' OR char = 'o' OR char = 'u' OR
+            char = 'A' OR char = 'E' OR char = 'I' OR char = 'O' OR char = 'U') THEN
+            vowel_counter := vowel_counter + 1;
+        END_IF
 
-function dotProduct(v1, v2) {
-    let ps = 0;
-    for (let i = 0; i < v1.length; i++) {
-        ps = ps + (v1[i] * v2[i]);
-    }
+        IF (char = ' ') THEN
+            word_counter := word_counter + 1;
+        END_IF
 
-    if (ps === 0) {
-        console.log("The vectors are orthogonal.");
-    } else {
-        console.log("The vectors are NOT orthogonal.");
-    }
-}
+        READ(char);
+    END_WHILE
+    length_counter := length_counter + 1;
+    WRITE("Length: ", length_counter);
+    WRITE("Words: ", word_counter);
+    WRITE("Vowels: ", vowel_counter);
+END
+En
